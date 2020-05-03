472e40fec50c0980c6a4fe3521da4b2f15d43514
Adean Ladia <adeanladia0129@gmail.com>
2020-05-03 21:02:34 +0700

    revert to point in mnt

--- upload.php
+++ upload.php
@@ -46,12 +46,12 @@
 		else { echo "Sorry, there was an error uploading ".basename($_FILES["targetFile"]["name"]).".<br>"; }
 
 		//echo "cmd$: ".("/Library/Frameworks/Python.framework/Versions/3.7/bin/python3 ./_evaluator/main.py --unpack \"".$base_filename.":".$modified_filename."\" --texts --colors --images --videos --animations <br>");
-		$output_unpack = shell_exec("/mnt/NARRA/_dev/others/Tool-V07/venv/bin/python ./_evaluator/main.py --unpack \"".$base_filename.":".$modified_filename."\" --texts --colors --images");
+		$output_unpack = shell_exec("/Library/Frameworks/Python.framework/Versions/3.7/bin/python3 ./_evaluator/main.py --unpack \"".$base_filename.":".$modified_filename."\" --texts --colors --images");
 		echo ($output_unpack."<br>");
 		
 		if (strpos($output_unpack, 'DONE') > 0) {
 			// proceed with summarize trigger
-			$output_summarize = shell_exec("/mnt/NARRA/_dev/others/Tool-V07/venv/bin/python ./_evaluator/main.py --summarize \"".$base_filename.":".$modified_filename."\"");
+			$output_summarize = shell_exec("/Library/Frameworks/Python.framework/Versions/3.7/bin/python3 ./_evaluator/main.py --summarize \"".$base_filename.":".$modified_filename."\"");
 			echo ($output_summarize."<br>");
 			if (strpos($output_summarize, 'DONE') > 0) {
 				echo "<br>redirect to <a href=\"results.php\">results</a> page";
